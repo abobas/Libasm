072461ea5b1ef59f81d6f9f006851f58f04c232b
Telly <abobas@student.codam.nl>
2020-05-14 21:23:50 +0200

    indentation

--- ft_write.s
+++ ft_write.s
@@ -6,22 +6,22 @@
 #    By: abobas <abobas@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/04/30 14:59:40 by abobas        #+#    #+#                  #
-#    Updated: 2020/05/14 21:11:17 by abobas        ########   odam.nl          #
+#    Updated: 2020/05/14 21:23:33 by abobas        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
-global  _ft_write
+global	_ft_write
 extern	___error
 	
-_ft_write:                                  ; rdi = file descriptor, rsi = string, rdx = byte count
-			mov         rax, 0x2000004      ; BSD / MacOS calling convention unlike Linux
+_ft_write:										; rdi = file descriptor, rsi = string, rdx = byte count
+			mov		 	rax, 0x2000004	; BSD / MacOS calling convention unlike Linux
 			syscall
-			jc         	error               ; error sets carry flag, rax = errno
+			jc		 	error				 		; error sets carry flag, rax = errno
 			ret
 error:
-			mov			r15, rax 			; save errno 
+			mov			r15, rax 				; save errno 
 			push		r15
-			call		___error			; retrieve address to errno
+			call		___error				; retrieve address to errno
 			pop			r15
 			mov			[rax], r15			; put errno in return value of __error (pointer to errno)
 			mov			rax, -1
